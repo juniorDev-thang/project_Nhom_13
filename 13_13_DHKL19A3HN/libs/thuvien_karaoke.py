a51874d1b545169b878f0f81f54cf05b5466bfde
NguyenPhuongNhi <npNhi.25174600181@sv.uneti.edu.vn>
2025-12-31 18:12:24 +0700

    cap nhat bai

--- 13_13_DHKL19A3HN/libs/thuvien_karaoke.py
+++ 13_13_DHKL19A3HN/libs/thuvien_karaoke.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import csv
-import os
-
-def khoi_tao_file():
-    filename = "ds_hoadon.csv"
-    if not os.path.exists(filename):
-        with open(filename, mode="w", newline="", encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerow(["MaHD", "TenKH", "SoLuong", "DonGia", "ThanhTien"])
-
-def luu_file(ds):
-    filename = "ds_hoadon.csv"
-    with open(filename, mode="a", newline="", encoding="utf-8") as f:
-        writer = csv.writer(f)
-        for hd in ds:
-            writer.writerow([hd["MaHD"], hd["TenKhach"], hd["LoaiPhong"], hd["SoGio"], hd["ThanhTien"]])
-
-def sap_xep(ds):
-    return sorted(ds, key=lambda x: x["ThanhTien"], reverse=True)
-=======
 def Nhap_Hoa_Don():
     ds = []
     while True:
@@ -76,4 +55,23 @@ def Tinh_Tien(hoadon):
 
     hoadon["ThanhTien"] = int(tong)
     return hoadon
->>>>>>> origin/Vu
+
+import csv
+import os
+
+def khoi_tao_file():
+    filename = "ds_hoadon.csv"
+    if not os.path.exists(filename):
+        with open(filename, mode="w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["MaHD", "TenKH", "SoLuong", "DonGia", "ThanhTien"])
+
+def luu_file(ds):
+    filename = "ds_hoadon.csv"
+    with open(filename, mode="a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for hd in ds:
+            writer.writerow([hd["MaHD"], hd["TenKhach"], hd["LoaiPhong"], hd["SoGio"], hd["ThanhTien"]])
+
+def sap_xep(ds):
+    return sorted(ds, key=lambda x: x["ThanhTien"], reverse=True)
