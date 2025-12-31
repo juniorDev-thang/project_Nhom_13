b3b2c857af938ac7327401d4c031007e90ad72e3
NguyenPhuongNhi <npNhi.25174600181@sv.uneti.edu.vn>
2025-12-31 18:03:42 +0700

    cap nhat bai nhi

--- 13_13_DHKL19A3HN/libs/thuvien_karaoke.py
+++ 13_13_DHKL19A3HN/libs/thuvien_karaoke.py
@@ -1,2 +1,19 @@
-def tinhtoan():
-    return a + b
+import csv
+import os
+
+def khoi_tao_file():
+    filename = "ds_hoadon.csv"
+    if not os.path.exists(filename):
+        with open(filename, mode="w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["MaHD", "TenKH", "SoLuong", "DonGia", "ThanhTien"])
+
+def luu_file(ds):
+    filename = "ds_hoadon.csv"
+    with open(filename, mode="a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for hd in ds:
+            writer.writerow([hd["MaHD"], hd["TenKhach"], hd["LoaiPhong"], hd["SoGio"], hd["ThanhTien"]])
+
+def sap_xep(ds):
+    return sorted(ds, key=lambda x: x["ThanhTien"], reverse=True)
